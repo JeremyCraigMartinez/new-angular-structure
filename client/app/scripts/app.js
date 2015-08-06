ac10dd1e2afb6110c15a8a75d7e6abf5168cb0a5
jeremy <jeremymartinez11@gmail.com>
2015-08-06 00:05:11 -0700

    graph accomplished, and a wated 2 hours on calendar

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,5 +1,26 @@
 'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
 angular.module('clientApp', [
-    'ui.router',
-    'cookiesProvider'
-]);
+	'ngSanitize',
+	'ngResource',
+	'ngAria',
+	'ngMessages',
+	'ngAnimate',
+	'ngTouch',
+  'ui.calendar',
+  'ui.bootstrap',
+  'ui.router',
+  'cookiesProvider',
+  'angular.morris-chart',
+  'ui.utils'
+])
+  .config(function (uiCalendarConfig) {
+	});
